e82fa8be2be63dba50a9fab2c1a871fbdc5daad3
Sebastien MAILLET <sebastien.maillet@gmail.com>
2025-06-24 11:22:53 +0200

    Affichage des produits, création de la recherche et du filtre

--- src/pages/produit/Produit.jsx
+++ src/pages/produit/Produit.jsx
@@ -1,40 +1,102 @@
 import React, { useEffect, useState } from 'react'
 import { Axios } from '../../services/ServiceAppel'
 import { Link } from 'react-router'
+import { TitrePage } from '../../composants/titrePage/TitrePage'
+import { Carte } from '../../composants/carte/Carte'
+import { Recherche } from '../../composants/recherche/Recherche'
+import { Filtre } from '../../composants/filtre/Filtre'
+import './Produit.css'
 
 export const Produit = () => {
-    const [produits, setProduits] = useState([]);
+	/* Déclaration des états nécessaires */
+	const [produits, setProduits] = useState([]);
+	const [typeProduits, setTypeProduits] = useState([]);
+	const [produitsRecherches, setProduitsRecherches] = useState([]);
+	const [produitsFiltres, setProduitsFiltres] = useState([]);
+	const [produitsAffiches, setProduitsAffiches] = useState([]);
+	const [listeAffichage, setListeAffichage] = useState([]);
 
-    const fetchProduits = () => {
-      try {
-        return Axios.get('/produits').then((res) => {
-        setProduits(res.data.member);
-        });
-      } catch (error) {
-        console.error('Error fetching produits:', error);
-      }
-    };
+	/* Fonction de récupération des produits */
+	const fetchProduits = () => {
+		try {
+			return Axios.get('/produits').then((res) => {
+				setProduits(res.data.member);
+				/* 				setProduitsRecherches(res.data.member);
+								setProduitsFiltres(res.data.member); */
+				setProduitsAffiches(res.data.member);
+			});
+		} catch (error) {
+			console.error('Error fetching produits:', error);
+		}
+	};
 
-    useEffect(() => {
-      fetchProduits();
-    },[])
+	/* Fonction de récupération des types de produits */
+	const fetchTypeProduits = () => {
+		try {
+			return Axios.get('/type_produits').then((res) => {
+				setTypeProduits(res.data.member);
+			});
+		} catch (error) {
+			console.error('Error fetching produits:', error);
+		}
+	};
 
-  return (
-    <div>
-        <Link to='/'>Accueil</Link>
-        <h1>Produit</h1>
-        {produits.map((produit) => (
-          <div key={produit.id}>
-            <h2>{produit.nom}</h2>
-            <p>{produit.description}</p>
-            <p>{produit.actif ? 'Actif' : 'Inactif'}</p>
-          </div>
-        ))
-        }
+	/* 	Récupérationdes produits et des types de produits */
+	useEffect(() => {
+		fetchProduits();
+		fetchTypeProduits();
+	}, []);
+
+	/* Filtre des produits en fonction de la recherche et des filtres */
+	useEffect(() => {
+		if (produitsRecherches.length == 0) {
+			if (produitsFiltres.length == 0) {
+				setProduitsAffiches(produits);
+			}
+			else {
+				setProduitsAffiches(produitsFiltres);
+			}
+		}
+		else {
+			if (produitsFiltres.length == 0) {
+				setProduitsAffiches(produitsRecherches);
+			}
+			else {
+				setProduitsAffiches(produitsRecherches.filter((produit) => produitsFiltres.includes(produit)));
+			}
+		}
+	}, [produitsRecherches, produitsFiltres]);
 
 
+	return (
+		<div>
+			<Link to='/'>Accueil</Link>
 
-    </div>
+			<TitrePage titre="Produits" classe="titreProduits" />
 
-  )
+			<div className="container">
+				{/* Section de recherche et filtres */}
+				<div className="rechercheFiltre">
+					<div className="recherche">
+						<h4 className="titreRecherche">Recherche</h4>
+						<Recherche liste={produits} setListe={setProduitsRecherches} />
+					</div>
+					<div className="filtre">
+						<h4 className="titreFiltre">Filtre</h4>
+						<Filtre typeProduits={typeProduits} liste={produits} setListe={setProduitsFiltres} />
+					</div>
+				</div>
+				{/* Grille de présentation des produits */}
+				<div className="grid-container">
+					{produitsAffiches.map((produit, index) => (
+						<div key={index} className={`grid-item ${produit.actif ? "visible" : "invisible"}`}>
+							<Carte nom={produit.nom} description={produit.description} typeProduit={produit.typeProduit.nom} actif={produit.actif} />
+						</div>
+					))
+					}
+				</div>
+			</div>
+
+		</div>
+	)
 }
