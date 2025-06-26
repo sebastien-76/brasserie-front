e07983f51ef4a8c88383ebf613bda604ad6ebb41
Sébastien <125682511+sebastien-76@users.noreply.github.com>
2025-06-26 18:36:58 +0200

    Update ServiceAppel.js

--- src/services/ServiceAppel.js
+++ src/services/ServiceAppel.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const Axios = axios.create({
 /* baseURL: "https://localhost:8000/api", */
-baseURL: "https://lbrasserie-back.sebastienmaillet.fr/api",
+baseURL: "https://brasserie-back.sebastienmaillet.fr/api",
 });
