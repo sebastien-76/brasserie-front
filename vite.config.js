d20d05c0791c22957786282381d706aaf867a5aa
Sebastien MAILLET <sebastien.maillet@gmail.com>
2025-07-07 10:15:55 +0200

    Ajout vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // options de configuration de vitest
+    environment: 'node',
+    globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
 })
